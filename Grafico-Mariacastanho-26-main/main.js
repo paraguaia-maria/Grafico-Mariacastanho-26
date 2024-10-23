7817997354975085c5696d5f711c139fda3397b8
paraguaia-maria <162311915+paraguaia-maria@users.noreply.github.com>
2024-10-23 11:04:44 -0300

    Add files via upload

---
+++ Grafico-Mariacastanho-26-main/main.js
@@ -0,0 +1,19 @@
+const grafico = document.getElementById("meuGrafico").getContext("2d");
+const meuGrafico = new Chart (grafico, {
+    type: "pie",
+data:{
+    labels:[
+        "Instagram",
+        "WhatsApp",
+        "Facebook",
+        "WeChat",
+        "TikTok",
+        "DouYin",
+        "Twitter",
+        "Telegram",
+        "FB Messenger",
+        "LINE"
+    ],
+}
+}
+)
